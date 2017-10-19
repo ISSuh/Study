b98f94a6e935c4deb604cf6ecd7fcdb8644bfd89
Suh In Seock <yoyus5@naver.com>
2017-10-19 17:35:09 +0900

    Add files via upload

---
+++ Linux_System_SW/Ch13.pipe.3.0-fifo/server.c
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <math.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+
+//#############################################################################
+// Request: Complete the following functions. 
+// Due    : Dec. 08, 2015 21:00
+//#############################################################################
+
+
+//#############################################################################
+// Complete this server program which acts as follows
+// - receive a string from a client through a named pipe [./my_fifo] 
+// - print the string  on the screen
+// - convert the string to UPPER case.
+// - print the result string on the screen.
+// - send the result string to the client through a named pipe [./my_fifo]
+
+
+/*
+
+step 1. [client] ---> "abc" ---> [server]
+step 2. [client] <--- "ABC" <--- [server]
+
+*/
+
+
+#define FIFO_NAME "./my_fifo"
+#define BUFFER_SIZE 100
+
+//#############################################################################
+int main(int argc, char **argv)
+{
+	int i = 0,r;
+    int pipe_fd[2];
+    int res=0;
+	int open_mode1 = O_WRONLY;
+	int open_mode2 = O_RDONLY;
+    char buffer[BUFFER_SIZE];
+	if (argc != 1)
+	{
+		printf("USAGE: ] ./server \n");
+		exit(0);
+	}
+
+    memset(buffer, '\0', sizeof(buffer));
+
+	if (access(FIFO_NAME, F_OK) == -1) {
+        res = mkfifo(FIFO_NAME, 0777);
+        if (res != 0) {
+            fprintf(stderr, "Could not create fifo %s\n", FIFO_NAME);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+	pipe_fd[0] = open(FIFO_NAME, open_mode2); // 읽기
+	
+	if(pipe_fd[0] == -1){ 
+		printf("Error!\n");
+		exit(EXIT_FAILURE);
+		}
+
+	res = read(pipe_fd[0], buffer, BUFFER_SIZE);
+	
+	printf("Server Input  Data : %s\n", buffer);
+
+	r = strlen(buffer);
+
+	for(i = 0 ; i < r ; i++)
+		buffer[i] = toupper(buffer[i]);
+
+	(void)close(pipe_fd[0]);
+
+	pipe_fd[1] = open(FIFO_NAME, open_mode1); // 쓰기
+
+	if(pipe_fd[1] == -1){ 
+		printf("Error!\n");
+		exit(EXIT_FAILURE);	
+		}
+
+	res = write(pipe_fd[1], buffer, BUFFER_SIZE); // res = write(pipe_fd[0], buffer, strlen(buffer)); 
+
+    if (res == -1) { 
+		fprintf(stderr, "Write error on pipe\n");
+		exit(EXIT_FAILURE);
+		}
+
+    (void)close(pipe_fd[1]);
+  
+
+    printf("Server Output  Data : %s\n", buffer);
+
+	sleep(1);
+	printf("\n\n");
+	printf("Server Process %d finished\n", getpid());
+  	printf("Server Execution finished successfully.... \n");
+    exit(EXIT_SUCCESS);
+
+}
