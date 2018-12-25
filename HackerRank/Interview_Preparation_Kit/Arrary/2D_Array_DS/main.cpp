bbf72be6165be6bedd1ed627c63645f951c12263
SIS <yoyus5@naver.com>
2018-12-26 00:33:29 +0900

    20181225

---
+++ HackerRank/Interview_Preparation_Kit/Arrary/2D_Array_DS/main.cpp
@@ -0,0 +1,44 @@
+nclude <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    int result = -9999999, sum = 0;
+
+    for(auto i = 1 ; i < arr.size() - 1 ; i++){
+        for(auto j = 1 ; j < arr.size() - 1 ; j++){
+            sum = arr[i-1][j-1] + arr[i-1][j] +arr[i-1][j+1] +arr[i][j] +arr[i+1][j-1] +arr[i+1][j] +arr[i+1][j+1];
+            
+            if(sum > result)
+                result = sum;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
