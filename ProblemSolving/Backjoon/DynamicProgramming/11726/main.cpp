924d90a82e29dd86650dbb90ba3cbd668242c625
SIS <yoyus5@naver.com>
2019-03-01 17:13:30 +0900

    20190221

--- ProblemSolving/Backjoon/DynamicProgramming/11726/main.cpp
+++ ProblemSolving/Backjoon/DynamicProgramming/11726/main.cpp
@@ -1,3 +1,9 @@
+/* 
+https://www.acmicpc.net/problem/11726
+
+2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+*/
+
 #include <iostream>
 #include <vector>
 
@@ -11,7 +17,7 @@ int main(){
     item[1] = 2;
 
     for(auto i = 2 ; i < item.size() ; i++)
-        item[i] = (item[i-1] +item[i-2] % 10007;
+        item[i] = (item[i-1] +item[i-2]) % 10007;
      
     std::cout << item[n-1] << std::endl;
 
--- ProblemSolving/Backjoon/DynamicProgramming/11727/main.cpp
+++ ProblemSolving/Backjoon/DynamicProgramming/11727/main.cpp
@@ -1,3 +1,9 @@
+/*
+https://www.acmicpc.net/problem/11727
+
+2×n 직사각형을 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+*/
+
 #include <iostream>
 #include <vector>
 
