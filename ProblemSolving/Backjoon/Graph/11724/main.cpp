2be198e115123dfef0473d7084d62bb298d24c8b
issuh <yoyus5@naver.com>
2019-03-23 18:12:28 +0900

    20190323

--- ProblemSolving/Backjoon/Graph/11724/main.cpp
+++ ProblemSolving/Backjoon/Graph/11724/main.cpp
@@ -1,7 +1,7 @@
 /*
-https://www.acmicpc.net/problem/1260
+https://www.acmicpc.net/problem/11724
 
-그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
 */
 
 #include <iostream>
